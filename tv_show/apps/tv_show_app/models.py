6eeb741d58db306db89d22b371646af5621bee2c
Jaime Campillay <jcampillayworks@gmail.com>
2021-05-06 10:48:38 -0400

    Django TV Show Validations basic

--- tv_show/apps/tv_show_app/models.py
+++ tv_show/apps/tv_show_app/models.py
@@ -1,5 +1,25 @@
+from __future__ import unicode_literals
+# import re
 from django.db import models
 from datetime import datetime, date
+
+class ShowManager(models.Manager):
+    def validator(self, postData):
+        errors = {}
+
+        if(len(postData['title'])) < 2:
+            errors['title'] = "El titulo debe tener a los menos 2 caractéres"
+        
+        if(len(postData['network'])) < 3:
+            errors['network'] = "El network debe tener a los menos 3 caractéres"
+        
+        if len(postData['release_date']) < 8:
+            errors['release_date'] = "Fecha lanzamiento no tiene largo correcto"
+        
+        if len(postData['description']) < 10:
+            errors['description'] = "La descripción debe tener a los menos 3 caractéres"
+        
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=250)
@@ -8,3 +28,4 @@ class Show(models.Model):
     description = models.CharField(max_length=500, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- tv_show/apps/tv_show_app/views.py
+++ tv_show/apps/tv_show_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-
 from .models import Show
+from django.contrib import messages
 
 def index(request):
     return redirect("/home")
@@ -15,8 +15,14 @@ def new_show(request):
     return render(request, "add_show.html")
 
 def create(request):
-    Show.objects.create(title=request.POST['title'], network=request.POST['network'],release_date=request.POST['release_date'], description=request.POST['description'])
-    return redirect('/home')
+    errors = Show.objects.validator(request.POST)
+    if len(errors)> 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/home/new_show')
+    show = Show.objects.create(title=request.POST['title'], network=request.POST['network'],release_date=request.POST['release_date'], description=request.POST['description'])
+    messages.success(request, "Show succesfullt created")
+    return redirect(f'/home/{show.id}')
 
 def show (request, id):
     ctx ={
@@ -36,9 +42,16 @@ def destroy(request, id):
 
 
 def update(request):
-    u = Show.objects.get(id=request.POST['id'])
-    u.title = request.POST['title']
-    u.network = request.POST['network']
-    u.release_date = request.POST['release_date']
-    u.save()
-    return redirect("/home/"+str(u.id))
+    errors = Show.objects.validator(request.POST)
+    if len(errors)>0:
+        for key, value in errors.items():
+            messages.error(request.value)
+        return redirectf(f'/home/{id}/edit')
+    else:
+        u = Show.objects.get(id=request.POST['id'])
+        u.title = request.POST['title']
+        u.network = request.POST['network']
+        u.release_date = request.POST['release_date']
+        u.description = request.POST['description']
+        u.save()
+        return redirect("/home/"+str(u.id))
